db3e73f20e6da0d8b7385fd924ac77fc84a21226
Maruf hasan <emarufhasan@gmail.com>
2018-01-29 16:18:33 +0600

    Updated

--- experiment_ci/application/views/components/privilege/privilege.php
+++ experiment_ci/application/views/components/privilege/privilege.php
@@ -1,3 +1,44 @@
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+    @media (min-width: 768px){
+        .form-horizontal .control-label {
+            text-align: left !important;
+        }
+    }
+    .panal-header {
+        background: rgb(240, 240,240);
+    }
+    .header .form-group {
+        margin-bottom: 0;
+        padding-left: 20px;
+        float: left;
+        width: 100%;
+        margin-right: 5px;
+    }
+    .header .form-group label {
+        position: relative;
+        border: 1px solid rgb(240, 240, 240);
+        display: block;
+        width: 100%;
+        padding-left: 15px;
+        line-height: 34px;
+        transition: all 0.3s ease-in-out;
+    }
+    .header .form-group label:hover {
+        box-shadow: 1px 1px 2px 3px rgb(210, 210, 210);
+    }
+    .header .form-group label .action {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        visibility: hidden;
+        transition: all 0.3s ease-in-out;
+    }
+    .header .form-group label:hover .action { visibility: visible;}
+    .header .form-group label input[type=checkbox] {margin: 0 10px 0 0;}
+    .header .form-group label input[type=text] {margin-top:5px;}
+    .header .form-group label input.form-control {width:85% !important;}
+</style>
 <div class="container-fluid">
     <div class="row">
     <?php echo $confirmation;
@@ -5,24 +46,82 @@
         <div class="panel panel-default">
              <!-- horizontal form -->
                 <?php
-                    $attr=array("class"=>"form-horizontal");
+                    $attr=array("class"=>"form");
                     echo form_open_multipart('', $attr);
                 ?>
 
             <div class="panel-heading panal-header">
-                <div class="panal-header-title pull-left">
-                    <div class="form-group">
-                        <label class="col-md-2 control-label"><input type="checkbox"> <h2>Privilege</h2> </label>
-                    </div>
+                <div class="panal-header-title">
+                    <h2>All Privilege </h2>
                 </div>
             </div>
 
             <div class="panel-body">
-                    <div class="form-group">
-                        <label class="col-md-2 control-label"><input type="chackbox"> Privilege </label>
+                <div class="row">
+                    <div class="col-md-5 header">
+                        <div class="panel panel-default">
+                            <div class="panel-heading panal-header">
+                                <div class="panal-header-title">
+                                    <label class="control-label">
+                                        <input type="checkbox">
+                                        <span style="font-size: 20px;">Aside One</span>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="panel-body">
+                                <div class="form-group">
+                                    <label class="control-label">
+                                      <input class="w3-check pull-left" type="checkbox" checked="checked">
+                                      <input type="text" value="View" class="form-control">
+                                        <span class="action btn-group">
+                                            <a href="#" class="btn btn-danger">Delete</a>
+                                            <a href="#" class="btn btn-info">Edit</a>
+                                            <a href="#" class="btn btn-success">view</a>
+                                        </span>
+                                    </label>
+                                    <label class="control-label">
+                                      <input class="w3-check pull-left" type="checkbox" checked="checked">
+                                      <input type="text" value="Edit" class="form-control">
+                                        <span class="action btn-group">
+                                            <a href="#" class="btn btn-danger"><input class="w3-check pull-left" type="checkbox" checked="checked">Delete</a>
+                                            <a href="#" class="btn btn-info"><input class="w3-check pull-left" type="checkbox" checked="checked">Edit</a>
+                                            <a href="#" class="btn btn-success"><input class="w3-check pull-left" type="checkbox" checked="checked">view</a>
+                                        </span>
+                                    </label>
+                                    <label class="control-label">
+                                      <input class="w3-check pull-left" type="checkbox" checked="checked">
+                                      <input type="text" value="Delete" class="form-control">
+                                        <span class="action btn-group">
+                                            <a href="#" class="btn btn-danger">Delete</a>
+                                            <a href="#" class="btn btn-info">Edit</a>
+                                            <a href="#" class="btn btn-success">view</a>
+                                        </span>
+                                    </label>
+                                    <label class="control-label">
+                                      <input class="w3-check pull-left" type="checkbox" checked="checked">
+                                      <input type="text" value="Main" class="form-control">
+                                        <span class="action btn-group">
+                                            <a href="#" class="btn btn-danger">Delete</a>
+                                            <a href="#" class="btn btn-info">Edit</a>
+                                            <a href="#" class="btn btn-success">view</a>
+                                        </span>
+                                    </label>
+                                    <label class="control-label">
+                                      <input class="w3-check pull-left" type="checkbox" checked="checked">
+                                      <input type="text" value="Edit" class="form-control">
+                                        <span class="action btn-group">
+                                            <a href="#" class="btn btn-danger">Delete</a>
+                                            <a href="#" class="btn btn-info">Edit</a>
+                                            <a href="#" class="btn btn-success">view</a>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="panel-footer">&nbsp;</div>
+                        </div>
                     </div>
-
-
+                </div>
                     
                 <?php echo form_close(); ?>
             </div>
--- rename_file/index.php
+++ rename_file/index.php
@@ -1,13 +1,108 @@
 <?php
-$files = glob("2017/*.*");
+//Student Array
+$students = array(
+"1"=>"180515",
+	"2"=>"180516",
+	"3"=>"180517",
+	"4"=>"180518",
+	"5"=>"180519",
+	"6"=>"180520",
+	"7"=>"180521",
+	"8"=>"180522",
+	"9"=>"180523",
+	"10"=>"180524",
+	"11"=>"180525",
+	"12"=>"180526",
+	"13"=>"180527",
+	"14"=>"180528",
+	"15"=>"180529",
+	"16"=>"180530",
+	"17"=>"180531",
+	"18"=>"180532",
+	"19"=>"180533",
+	"20"=>"180534",
+	"21"=>"180535",
+	"22"=>"180536",
+	"23"=>"180537",
+	"24"=>"180538",
+	"25"=>"180539",
+	"26"=>"180540",
+	"27"=>"180541",
+	"28"=>"180542",
+	"29"=>"180543",
+	"30"=>"180544",
+	"31"=>"180545",
+	"32"=>"180546",
+	"33"=>"180547",
+	"34"=>"180548",
+	"35"=>"180549",
+	"36"=>"180550",
+	"37"=>"180551",
+	"38"=>"180552",
+	"39"=>"180553",
+	"40"=>"180554",
+	"41"=>"180555",
+	"42"=>"180556",
+	"43"=>"180557",
+	"44"=>"180558",
+	"45"=>"180559",
+	"46"=>"180560",
+	"47"=>"180561",
+	"48"=>"180562",
+	"49"=>"180563",
+	"50"=>"180564",
+	"51"=>"180565",
+	"52"=>"180566",
+	"53"=>"180567",
+	"54"=>"180568",
+	"55"=>"180569",
+	"56"=>"180570",
+	"57"=>"180571",
+	"58"=>"180572",
+	"59"=>"180573",
+	"60"=>"180574"
+);
+
+$files = glob("src/*.*");
 //print_r($files);
-foreach ($files as $key => $name) {
-	//echo $name . "<br/>";
-	$des = str_shuffle("Student".rand(10000000,99999999).strtotime("now")).".jpg";
-	if(copy($name, "new/".$des)){
+foreach ($students as $roll => $id) {
+//foreach($files as $key => $src){
+	//$file_name = pathinfo($src,PATHINFO_BASENAME);
+	//$extension = pathinfo($src,PATHINFO_EXTENSION);
+	//$filename = str_replace("..",".",$file_name);
+	//echo $extension . "<br/>";
+	//$name = str_shuffle("Student".rand(10000000,99999999).strtotime("now")).".jpg";
+	/*
+	if(copy($name, "new/".$name)){
 		$file_name = pathinfo($name,PATHINFO_BASENAME);
 		$file_name = explode(".", $file_name);
 		$roll = $file_name[0];
 		echo $roll.",".$des.PHP_EOL;
-	};
+	}*/
+	
+	
+	//Main
+	$src = "src/".$roll.".jpg";
+	if(is_file($src)){
+		if(copy($src,"des/".$id.".jpg")){
+			unlink($src);
+			echo $roll."\t".$id."<br/>";
+		}		
+	}
+	//Main
+	
+	/*
+	//Removing dot
+	
+	$file_name = pathinfo($src,PATHINFO_BASENAME);
+	$extension = pathinfo($src,PATHINFO_EXTENSION);
+	$filename = str_replace("6B-","",$file_name);
+	if(copy($src, "des/".$filename)){
+		unlink($src);
+		echo "Renamed"."<br>";
+	}else{
+		echo "Not Working";
+	}*/
+	
 }
+
